e56fdeda57a0bfd34fee895f93a62c94c76799d0
Paul Friederichsen <floogulinc@gmail.com>
2019-12-11 04:20:21 -0600

    Update

--- process-pages.r
+++ process-pages.r
@@ -2,15 +2,18 @@ library(tidyverse)
 library(rvest)
 library(rlist)
 
+index <- 1:253772
+index.nomissing <- list.filter(index, class(html_node(read_html(paste("site-dump-1/id.php_id=", ., sep="")), "#paddingWrapper")) != "xml_missing")
+
+# dont wan't to rerun this on accident
+#saveRDS(index.nomissing, "indexnomissing.Rda") 
+
+
 parse.id = function(addr) {
   return(sub(".*id.php\\?id=", '', addr) %>% strtoi)
 }
 
-
-index <- 19964:19966
-index.nomissing <- list.filter(index, class(html_node(read_html(paste("site-dump-1/id.php_id=", ., sep="")), "#paddingWrapper")) != "xml_missing")
-
-test <- paste("site-dump-1/id.php_id=", index.nomissing, sep = "") %>% map(read_html) %>% map(html_node, "#paddingWrapper") %>% {
+system.time(site.data <- paste("site-dump-1/id.php_id=", index.nomissing, sep = "") %>% map(read_html) %>% map(html_node, "#paddingWrapper") %>% {
   tibble(
     id = index.nomissing,
     name = map(., html_node, "h2") %>% map_chr(html_text, trim = TRUE),
@@ -27,6 +30,6 @@ test <- paste("site-dump-1/id.php_id=", index.nomissing, sep = "") %>% map(read_
     students.raw = map(., html_node, "table") %>% map_if(~ class(.) != "xml_missing" ,html_table),
     students.id = map(., html_node, "table") %>% map(html_nodes, "a") %>% map(html_attr, "href") %>% map(map, parse.id)
   )
-}
+})
 
-
+saveRDS(site.data, "sitedata.Rda")
